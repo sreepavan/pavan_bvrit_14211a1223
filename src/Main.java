198fa1e82ce19766874aab66e2f46e3af2f65c5a
sreepavan <sreepavan77@gmail.com>
2017-09-12 14:00:16 +0530

    first

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+public class Main {
+
+	private static Date yourDate;
+
+	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+		java.util.Date yourDate = sdf.parse("1992-07-26");
+		}catch(Exception e) {};
+		/*Student stu1=new Student(1,"ram",yourDate,95);
+		Student stu2=new Student(2,"ram",yourDate,95);
+		Student stu3=new Student(3,"ram",yourDate,95);
+		Student stu4=new Student(4,"ram",yourDate,95);
+		*/
+
+		Student[] studentArray = new Student[7];
+		
+		studentArray[0]=new Student(1,"ram",yourDate,95);
+		studentArray[1]=new Student(2,"ram",yourDate,95);
+		studentArray[2]=new Student(3,"ram",yourDate,95);
+		studentArray[3]=new Student(4,"ram",yourDate,95);
+		
+		
+		
+		
+		
+		
+		StudentGroup s=new StudentGroup(5);
+		s.getStudents();
+		s.setStudents(studentArray);
+		
+		
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
